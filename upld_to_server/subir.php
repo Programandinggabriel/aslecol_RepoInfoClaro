00e47c134989b7c094e7ea26a7e91b9f2c02c4af
Gabriel Santiago <103392402+Programandinggabriel@users.noreply.github.com>
2022-10-08 00:30:32 -0500

    subir.php

--- upld_to_server/subir.php
+++ upld_to_server/subir.php
@@ -1,5 +1,5 @@
 <?php
     $nombre_temporal = $_FILES['archivo']['tmp_name'];
     $nombre = $_FILES['archivo']['name'];
-    move_uploaded_file($nombre_temporal, "../xlsx/" . $nombre);
+    move_uploaded_file($nombre_temporal, '../xlsx/'.$nombre);
 ?>
